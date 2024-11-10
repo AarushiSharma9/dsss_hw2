59d1a626d9f15a466fe8f295c81ac0ecbda66ebd
aarushi.sharma0912 <aarushi.sharma0912@gmail.com>
2024-11-10 15:31:24 +0100

    Improved code: comments, readability, error handling

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,46 @@
 import random
 
 
-def function_A(min, max):
+def random_int(min, max):
     """
-    Random integer.
+    Generates a random integer from the min, max range.
     """
     return random.randint(min, max)
 
 
-def function_B():
+def random_op(): # chooses a random airthmetic operator
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+def problem(n1, n2, o): #generate a math problem with n1: number 1; n2: number 2 & o: operator ('+', '-', '*').
+    p = f"{n1} {o} {n2}" #the math problem
+    if o == '+': a = n1 + n2
+    elif o == '-': a = n1 - n2
     else: a = n1 * n2
-    return p, a
+    return p, a #p: problem ; a: answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    score = 0
+    total_ques = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(total_ques):
+        n1 = random_int(1, 10); n2 = random_int(1, 5); o = random_op()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
+        p, ANSWER = problem(n1, n2, o)
+        print(f"\nQuestion: {p}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += -(-1)
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{total_ques}")
 
 if __name__ == "__main__":
     math_quiz()
