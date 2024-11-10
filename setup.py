b5be2885aacb961329a5b1ab35c35f9e7b6cbeee
aarushi.sharma0912 <aarushi.sharma0912@gmail.com>
2024-11-10 21:17:36 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="math_quiz",
+    version= "0.1",
+    author="Aarushi",
+    author_email="aarushi.sharma@fau.de",
+    packages=find_packages(),
+    install_requires=[],
+
+    )
